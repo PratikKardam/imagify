9997e9710000255c20545c35589f4145b4886c4a
wikiman <140505644+PratikKardam@users.noreply.github.com>
2025-07-18 13:14:13 +0530

    complete

--- server/server.js
+++ server/server.js
@@ -9,12 +9,19 @@ const port = process.env.PORT || 4000
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist')); // or 'build', depending on Vite output
+
 app.use(cors())
 await connectDB()
+
 
 
 app.use('/api/user', userRouter)
 app.use('/api/image', imageRouter)
 app.get('/',(req,res)=> res.send("API WORKING"))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
 
 app.listen(port, ()=> console.log("Server running on port " + port))
